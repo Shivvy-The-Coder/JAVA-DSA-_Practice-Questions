1c8a0814e82d40e8b93c6c60a91b7a086adfd4c6
Shivvy-The-Coder <krshivam455@gmail.com>
2024-07-01 13:31:30 +0530

    2d ArrayList added

--- combination_of_coins_2dList.java
+++ combination_of_coins_2dList.java
@@ -1,19 +1,21 @@
+
 import java.util.*;
-public class combination_of_Coins 
-{
+public class combination_of_coins_2dList {
     public static void main(String[] args)
     {
         int []coin={2,3,5};
         int amount=7;   
+        List<List<Integer>> ansr = new ArrayList<>(); 
         List<Integer> ll = new ArrayList<>();
-        print_combintaion(coin, amount," ",0,ll);
+        print_combintaion(coin, amount," ",0,ll,ansr);
+        System.out.println(ansr);
     }
-    public static void print_combintaion(int [] coins , int amount , String ans,int start, List<Integer> ll)
+    public static void print_combintaion(int [] coins , int amount , String ans,int start, List<Integer> ll, List<List<Integer>> ansr)
 
         {
             if(amount==0)
                 {
-                    System.out.println(ll);
+                    ansr.add(new ArrayList<>(ll));
                     return;
                 } 
             if(amount<0)
@@ -22,7 +24,7 @@ public class combination_of_Coins
             for(int i=start;i<coins.length;i++)
                 {
                     ll.add(coins[i]);
-                    print_combintaion(coins, amount-coins[i], ans+coins[i],i,ll);
+                    print_combintaion(coins, amount-coins[i], ans+coins[i],i,ll,ansr);
                     ll.remove(ll.size()-1);
                 }
                 
