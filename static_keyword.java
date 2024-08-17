501691ed18c302b19809ca2800bd1017bea6a897
Shivvy-The-Coder <krshivam455@gmail.com>
2024-08-17 11:29:15 +0530

    codes added

---
+++ static_keyword.java
@@ -0,0 +1,20 @@
+/**
+ * static_keyword
+ */
+public class static_keyword {
+
+    String name="Shivam";
+    int age=19;
+    public void answer()
+    {
+        System.out.println("My name is "+name +" and age is "+age);
+    }
+}
+class Main
+{
+    public static void main(String[] args) 
+    {
+        static_keyword obj = new static_keyword();
+        obj.answer();    
+    }
+}
