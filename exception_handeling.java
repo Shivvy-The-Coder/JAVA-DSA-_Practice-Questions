ede354a67d0fef551c1ea0d6fab02ce966e4550b
Shivvy-The-Coder <krshivam455@gmail.com>
2024-08-18 22:53:45 +0530

    codes updated

--- exception_handeling.java
+++ exception_handeling.java
@@ -10,10 +10,29 @@ public class exception_handeling {
         {
             this.name = name;
         }
-    public void setage(int age)
-     {
-        this.age=age;
-     }    
+
+    public void setage(int age)throws Exception
+        {  try
+            {
+                if(age<0)
+                throw new Exception("You entered a age less than 0");
+            else
+                this.age=age;
+            }
+            catch( Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }  
+
+    // public void setage(int age)throws Exception
+    //  {
+    //     if(age<0)
+    //         throw new Exception("You entered a age less than 0");
+    //     else
+    //         this.age=age;
+    //  }    
      public int getAge() {
          return age;
      }
@@ -23,9 +42,10 @@ public class exception_handeling {
 
 }
 
-class main
+class Dmain
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception
+    {
         exception_handeling obj = new exception_handeling();
         obj.setage(-9);
         obj.setname("Shubham");
