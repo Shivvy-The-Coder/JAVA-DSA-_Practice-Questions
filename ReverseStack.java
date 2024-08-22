d21dd7272a5476a7365c52a784e988f6c2e0b245
Shivvy-The-Coder <krshivam455@gmail.com>
2024-08-22 22:34:53 +0530

    reversing of a Stack code added

---
+++ ReverseStack.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+/**
+ * ReverseStack
+ */
+public class ReverseStack {
+
+    public static void main(String[] args) 
+    {
+        Stack <Integer> st = new Stack<>();
+        st.push(2);
+        st.push(1);
+        st.push(3);
+        st.push(5);
+        st.push(7);
+        System.out.println(st);
+        Reverse(st);
+        System.out.println(st);     
+    }
+    public static void Reverse(Stack <Integer> st)
+        {
+            if(st.isEmpty())
+                return;
+            int x=st.pop();
+            Reverse(st);
+            addLast(st, x);
+        }
+    public static void addLast(Stack <Integer> st , int item)
+        {
+            if(st.isEmpty())
+                {
+                    st.push(item);
+                    return;
+                }
+            int x=st.pop();
+            addLast(st, item);
+            st.push(x);
+        }
+}
