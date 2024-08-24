3073274438c47b9735d8b53f2ef54d38fbb01256
Shivvy-The-Coder <krshivam455@gmail.com>
2024-08-24 12:27:02 +0530

    DI code for finding smallest number using stack has been added

---
+++ SmallestNumber.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String smallestNumber(String pattern) 
+    {
+        Stack <Integer> st = new Stack<>();
+        int ans[]= new int[pattern.length()+1];
+        int count=1;
+        for (int i=0;i<=pattern.length();i++)
+            {
+                if(i==pattern.length() || pattern.charAt(i)=='I')
+                    {
+                        ans[i]=count;
+                        count++;
+                        while(!st.isEmpty())
+                            {
+                                ans[st.pop()]=count;
+                                count++;
+                            }
+                    }
+                else
+                    {   
+                        st.push(i);
+                    }
+
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i=0;i<ans.length;i++)
+                sb.append(ans[i]);
+        return sb.toString();
+    }
+}
