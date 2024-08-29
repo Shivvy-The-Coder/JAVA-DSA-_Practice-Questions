9626610284c8a46e66553eb1d2ae7b7a7dc0fcdf
Shivvy-The-Coder <krshivam455@gmail.com>
2024-08-30 00:16:37 +0530

    maximum rectangle code has been added

---
+++ Stack_Codes/maximum_Area.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+/**
+ * maximum_Area
+ */
+public class maximum_Area {
+
+    public static void main(String[] args) {
+        int arr[]={2,3,5,4,6,1,7};
+        Stack <Integer> st = new Stack<>();
+        int area=0;
+        for (int i=0;i<arr.length;i++)
+            {
+                while(!st.isEmpty() && arr[i]<arr[st.peek()])
+                    {
+                        int h=arr[st.pop()];
+                        int r=i;
+                        if(!st.isEmpty())
+                            {
+                                int l=st.peek();
+                                area=Math.max(area,h*(r-l-1));
+                            }
+                        else
+                            {
+                                area=Math.max(area,h*r);
+                            }
+                    }
+                    st.push(i);
+            }
+        System.out.println(area);
+    }
+}
