1e70db84915a7b423ac0aa79bad3bdfecc7f8699
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-19 23:51:32 +0530

    Binary tree code  updated

--- Trees/Binary Tress/Trees/BinaryTree.java
+++ Trees/Binary Tress/Trees/BinaryTree.java
@@ -58,4 +58,25 @@ class BinaryTree
             Display(nn.Right);
            
         }
+    public int max()
+        {return  max(this.root);
+        }
+    private int max (Node nn)
+        {
+            if(nn ==null)
+                return Integer.MIN_VALUE;
+            int lmax = max(nn.left);
+            int rmax = max(nn.Right);
+            return  Math.max(nn.val,Math.max(lmax,rmax));
+        }
+    public int  min()
+    {return  min(this.root);}
+    private int min(Node nn)
+        {
+            if(nn ==null)
+                return Integer.MAX_VALUE;
+            int lmin = min(nn.left);
+            int rmin = min(nn.Right);
+            return Math.min(nn.val,Math.min(lmin,rmin));
+        }
 }   
--- Trees/client_Binary.java
+++ Trees/client_Binary.java
@@ -4,6 +4,8 @@ class client_Binary
     public static void main(String[] args) {
         BinaryTree obj =new BinaryTree();
         obj.Display();
+        System.out.println("Maximum  value of the given Binary Tree is "+obj.max());
+        System.out.println("Maximum  value of the given Binary Tree is "+obj.min());
     }
 }
 // 10 true 20 true 40 false false true 50 false false true 30 false true 60 true 70 false false false
