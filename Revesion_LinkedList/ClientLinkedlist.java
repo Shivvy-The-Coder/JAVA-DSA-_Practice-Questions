11b186db57bff5516faa6e287be4baeea5b0753a
Shivvy-The-Coder <krshivam455@gmail.com>
2025-01-21 17:19:00 +0530

    codes has been updated

--- Revesion_LinkedList/ClientLinkedlist.java
+++ Revesion_LinkedList/ClientLinkedlist.java
@@ -1,9 +1,10 @@
 package Revesion_LinkedList;
+
 public class ClientLinkedlist {
-        public static void main(String[] args) {
-            LinkedList ll = new LinkedList();
-            ll.addFirst(2);
-            ll.addFirst(3);
-            ll.Display(null);
-        }    
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.Display(null);
+    }
 }
--- Revesion_LinkedList/LinkedList.java
+++ Revesion_LinkedList/LinkedList.java
@@ -1,8 +1,7 @@
 package Revesion_LinkedList;
-class LinkedList
-{
-   public class Node 
-    {
+
+class LinkedList {
+    public class Node {
         int val;
         Node next;
     }
@@ -11,39 +10,32 @@ class LinkedList
     private Node Tail;
     int size;
 
+    public void addFirst(int num) {
+        Node nn = new Node();
+        nn.val = num;
 
-    public void addFirst(int num)
-        {
-            Node nn = new Node();
-            nn.val=num;
-
-            if(size==0)
-                {
-                    Head=nn;
-                    Tail=nn;
-                    size++;
-                }
-            else
-                {
-                    nn.next=Head;
-                    Head=nn;
-                }
-
+        if (size == 0) {
+            Head = nn;
+            Tail = nn;
+            size++;
+        } else {
+            nn.next = Head;
+            Head = nn;
         }
-    
-        public void Display(Node nn)
-            {
-                if(size==0)
-                   { System.out.println("Linked LIsr is Empty");
-                return;}
-                Node Temp=Head;
 
-                
-                while(Temp!=null)
-                    {
-                        System.out.print(Temp.val);
-                        Temp=Temp.next;
-                    }
-            }
+    }
+
+    public void Display(Node nn) {
+        if (size == 0) {
+            System.out.println("Linked LIsr is Empty");
+            return;
+        }
+        Node Temp = Head;
+
+        while (Temp != null) {
+            System.out.print(Temp.val);
+            Temp = Temp.next;
+        }
+    }
 
 }
