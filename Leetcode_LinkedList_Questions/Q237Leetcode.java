2c6df40f63b0f19aca3d0c5c0ad66f2979155651
Shivvy-The-Coder <krshivam455@gmail.com>
2025-01-25 03:40:46 +0530

    updated

---
+++ Leetcode_LinkedList_Questions/Q237Leetcode.java
@@ -0,0 +1,22 @@
+package Leetcode_LinkedList_Questions;
+
+public class Q237Leetcode {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public void deleteNode(ListNode node) {
+            ListNode ahead = node.next;
+            node.val = ahead.val;
+            node.next = ahead.next;
+            ahead = null;
+        }
+    }
+
+}
