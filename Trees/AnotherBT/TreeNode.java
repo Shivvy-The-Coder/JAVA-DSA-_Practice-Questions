d57cb05a0dfe15fd6d240837fbcbba5057613410
Shivvy-The-Coder <krshivam455@gmail.com>
2025-01-31 00:22:11 +0530

    check and tell if there exist a pathe which will target to sum from root to leaf node

---
+++ Trees/AnotherBT/TreeNode.java
@@ -0,0 +1,37 @@
+package AnotherBT;
+
+ class Q112Leetcode {
+
+   public boolean hasPathSum(TreeNode root, int targetSum) {
+
+      if (root == null)
+         return false;
+
+      if (root.left == null && root.right == null) {
+         if (targetSum - root.val == 0)
+            return true;
+      }
+
+      boolean leftsum = hasPathSum(root.left, targetSum - root.val);
+      boolean rightsum = hasPathSum(root.right, targetSum - root.val);
+
+      return leftsum || rightsum;
+
+   }
+}
+
+
+
+   public class TreeNode {
+   int val;
+   TreeNode left;
+   TreeNode right;
+   TreeNode() {}
+   TreeNode(int val) { this.val = val; }
+   TreeNode(int val, TreeNode left, TreeNode right) {
+   this.val = val;
+   this.left = left;
+   this.right = right;
+   }
+   }
+
