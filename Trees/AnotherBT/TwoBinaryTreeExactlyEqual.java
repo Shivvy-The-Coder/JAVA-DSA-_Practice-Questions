bc3d9dd4e46ee8f8922f793c45718e8f1c1303e5
Shivvy-The-Coder <krshivam455@gmail.com>
2025-01-30 12:18:29 +0530

    codes of two tree being exactly equal has been added

---
+++ Trees/AnotherBT/TwoBinaryTreeExactlyEqual.java
@@ -0,0 +1,39 @@
+package AnotherBT;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class TwoBinaryTreeExactlyEqual {
+
+    public boolean checkSymmetric(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null && root2 == null)
+            return true;
+        if (root1 == null || root2 == null)
+            return false;
+
+        if (root1.val != root2.val)
+            return false;
+
+        boolean left = checkSymmetric(root1.left, root2.left);
+        boolean right = checkSymmetric(root1.right, root2.right);
+
+        return left && right;
+    }
+}
