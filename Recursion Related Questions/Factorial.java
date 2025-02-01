ac2ba9e418545ed0cd3f4e82a76895b577f1a0a5
Shivvy-The-Coder <krshivam455@gmail.com>
2025-02-01 23:32:05 +0530

    factorial scond code has been added

--- Recursion Related Questions/Factorial.java
+++ Recursion Related Questions/Factorial.java
@@ -3,11 +3,20 @@ public class Factorial
     public static void main(String []args)
         {
             System.out.println(factorial(5));
+            System.out.println(factorial(0))
         }
     public static int factorial(int n)
         {
             if(n==0)
                 return 1;
             return n*factorial(n-1);
+            
+        }
+    public static int factorialcode2(int n)
+        {
+            if(n==0)
+                return 1;
+            int fn = factorial(n-1);
+            return n*fn;
         }
 }
