68cc88bf9cfd3ff3e740386271c858adf642da3b
Shivvy-The-Coder <krshivam455@gmail.com>
2025-02-03 15:22:21 +0530

    Binary Search Tree code has been intialized

---
+++ Trees/BinarySearchTree/Binary_Search_Tree.java
@@ -0,0 +1,23 @@
+package BinarySearchTree;
+
+public class Binary_Search_Tree {
+    
+    class Node 
+    {
+        int val;
+        Node left;
+        Node right;
+    }
+    private Node root;
+
+    public Binary_Search_Tree( int [] in)
+        {
+            root = CreateTree(in, 0 ,in.length-1 );
+                
+        }
+    private Node CreateTree(int[] in, int si, int ei)
+    {
+        
+        return null;
+    }
+}
