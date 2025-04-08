07a5c4c37cf33a06673930b390aaaa6c2ff96dba
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-04-08 23:45:23 +0530

    added

---
+++ Array2/TrappingRainWater.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextLine();
+        int arr[] = new int[n];
+
+    }
+}
